0565a815413f553e40921fc7a88ebddd5f85bf6c
k173man <shane.t.reed@gmail.com>
2015-04-08 15:51:23 -0400

    Project 1 R files and PNG files

---
+++ plot4.R
@@ -0,0 +1,27 @@
+library(sqldf)
+
+f <- file("data/household_power_consumption.txt")
+hpc <- sqldf(
+    "select * from f where Date = '1/2/2007' or Date = '2/2/2007'", 
+    dbname = tempfile(), 
+    file.format = list(sep = ";", header = T, row.names = F)
+)
+close(f)
+
+hpc$datetime <- strptime(paste(hpc$Date, hpc$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+
+png(filename = "plot4.png")
+par(mfrow = c(2, 2))
+# plot 1 of 4
+with(hpc, plot(datetime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+# plot 2 of 4
+with(hpc, plot(datetime, Voltage, type = "l", ylab = "Voltage"))
+# plot 3 of 4
+with(hpc, plot(datetime, Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "n"))
+with(hpc, lines(datetime, Sub_metering_1, col = "black"))
+with(hpc, lines(datetime, Sub_metering_2, col = "red"))
+with(hpc, lines(datetime, Sub_metering_3, col = "blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, bty = "n")
+# plot 4 of 4
+with(hpc, plot(datetime, Global_reactive_power, type = "l"))
+dev.off()
